efe4de4a52adc0d3539b8a72ea800df5da4e385e
hossein khanpour <hossein.khanpour@yahoo.com>
2022-05-26 15:45:19 +0430

    100%

--- java.js
+++ java.js
@@ -1,3 +1,21 @@
-function greet(){
+// select
 
+let element = document.body.querySelector(".number")
+
+// count
+
+function get_count(){
+let current = element.textContent
+current = Number(current);
+// console.log(current);
+return current
 }
+
+// update
+
+function increment() {
+    let current = get_count();
+    let next = current + 1;
+    element.textContent = next;
+}
+setInterval(increment,1000)
