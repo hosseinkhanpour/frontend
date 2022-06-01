dd1dc935e7c34f587af6e9b58189f0caf8505f52
hossein khanpour <hossein.khanpour@yahoo.com>
2022-06-01 12:55:08 +0430

    tab-java

--- java.js
+++ java.js
@@ -18,4 +18,26 @@ function increment() {
     let next = current + 1;
     element.textContent = next;
 }
-setInterval(increment,1000)
+setInterval(increment,500)
+////////////////////////////////////////////////////
+
+let developer_select = document.body.querySelectorAll(".developers-select")
+let developer_info = document.body.querySelectorAll(".developers-info")
+
+developer_select.forEach(function tab_interator(item, index) {
+    item.addEventListener("click", function onclick(e) {
+        developer_select.forEach(function remove_classes(element){
+          element.classList.remove("select");  
+        });
+        let select_element = e.target;
+        select_element.classList.add("select");
+
+        developer_info.forEach(function  show_class_info(element) {
+            element.classList.remove("show");
+        });
+        let add_show = developer_info[index];
+add_show.classList.add("show");
+
+    });
+});
+
